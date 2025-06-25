d4b00f2139c396a1a07d45716b82b0ef33a5bd7c
axlock <axelitotuloko@outlook.com>
2025-06-25 10:50:18 -0500

    Update Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -4,18 +4,18 @@ public class Jugador {
     private int ataque;
     private int puntaje;
     public Jugador(String nombre, int salud, int ataque) {
-        this.nombre = nombre;
-        this.salud = salud;
-        this.ataque = ataque;
-        this.puntaje = 0;
+        this.nombre =nombre;
+        this.salud =salud;
+        this.ataque =ataque;
+        this.puntaje=0;
     }
     public void atacar() {
         System.out.println(nombre+" provoco "+ataque+" daño");
     }
     public void recibirDanio(int cantidad) {
-        salud -= cantidad;
-        if (salud < 0) salud = 0;
-        System.out.println(nombre + " recibió " + cantidad + " de daño.);}
+        salud -=cantidad;
+        if (salud<0) salud=0;
+        System.out.println(nombre+" recibió "+cantidad+" de daño".);}
     public boolean estaVivo() {
         return salud > 0;}
     public String getNombre() {
