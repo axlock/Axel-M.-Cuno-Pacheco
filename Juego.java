cbf412c4542e8a17e2ac6f107394e2733ac305bf
axlock <axelitotuloko@outlook.com>
2025-06-25 10:38:51 -0500

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -17,20 +17,20 @@ public class Juego {
         enemigos = generarEnemigos(3);
         System.out.println("¡Juego iniciado!");}
     public void ejecutarTurno() {
-        System.out.println("Turno " + turno + " ejecutado.");
+        System.out.println("Turno "+turno+" ejecutado.");
         turno++;}
     public void verificarFinDelJuego() {
         juegoTerminado = false;}
     public void mostrarEstado() {
-        System.out.println("Jugador: " + jugador.getNombre() + ", Salud: " + jugador.getSalud());
-        for (int i = 0; i < enemigos.length; i++) {
-            System.out.println("Enemigo " + i + ": " + enemigos[i].getTipo() + ", Salud: " + enemigos[i].getSalud());
+        System.out.println("Jugador: "+jugador.getNombre()+", Salud: "+jugador.getSalud());
+        for (int i=0;i<enemigos.length;i++) {
+            System.out.println("Enemigo "+i+": "+enemigos[i].getTipo()+",Salud: "+enemigos[i].getSalud());
         }
     }
     private Enemigo[] generarEnemigos(int cantidad) {
         Enemigo[] lista = new Enemigo[cantidad];
-        for (int i = 0; i < cantidad; i++) {
-            lista[i] = new Enemigo("Tipo" + (i + 1), 30, 10);
+        for (int i=0;i<cantidad;i++) {
+            lista[i]=new Enemigo("Tipo"+(i+1),30,10);
         }
         return lista;
     }
