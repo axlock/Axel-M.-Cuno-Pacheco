4b0762cdf9dc6b64a9c8966359b66be2ad79b038
axlock <axelitotuloko@outlook.com>
2025-06-25 10:41:49 -0500

    Update Enemigo.java

--- Enemigo.java
+++ Enemigo.java
@@ -3,21 +3,21 @@ public class Enemigo {
     private int salud;
     private int ataque;
     public Enemigo(String tipo, int salud, int ataque) {
-        this.tipo = tipo;
-        this.salud = salud;
-        this.ataque = ataque;
+        this.tipo=tipo;
+        this.salud=salud;
+        this.ataque=ataque;
     }
     public void atacar() {
-        System.out.println("El enemigo (" + tipo + ") ataca con " + ataque + " de daño.");
+        System.out.println("El enemigo ("+tipo+") ataca ocasionado"+ataque+" de daño.");
     }
     public void recibirDanio(int cantidad) {
-        salud -= cantidad;
-        if (salud < 0) salud = 0;
-        System.out.println("El enemigo (" + tipo + ") recibió " + cantidad + " de daño. Salud restante: " + salud);}
+        salud -=cantidad;
+        if (salud<0) salud=0;
+        System.out.println("El enemigo ("+tipo+") recibió "+cantidad+" de daño. Salud restante: "+salud);}
     public boolean estaVivo() {
         return salud > 0;}
     public void mostrarEstado() {
-        System.out.println("Enemigo (" + tipo + ") - Salud: " + salud + " | Ataque: " + ataque);}
+        System.out.println("Enemigo ("+tipo+") - Salud: "+salud+" | Ataque: "+ataque);}
     public String getTipo() {
         return tipo;}
     public int getSalud() {
